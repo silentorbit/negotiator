4361da2e5fb7d8e7413d03bd693dc91c0dfe8415
Peter Hultqvist <phq@silentorbit.com>
2011-12-17 03:42:02 +0100

    Accept minimization

--- script/background.js
+++ script/background.js
@@ -1,24 +1,31 @@
 
+
+//Requests without any matching filter
+var TrackedRequests = {};
+
+//Track filter for every chrome request
+//Written in onBeforeSendHeaders and used in onHeadersReceived
+var requestFilter = {};
+
+chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, {urls: ["<all_urls>"]}, ["blocking"]);
+chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders, {urls: ["<all_urls>"]}, ["requestHeaders", "blocking"]);
+chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, {urls: ["<all_urls>"]}, ["responseHeaders"]);
+
+//Counter of blocked requests, shown in the badge.
 var blocked = 0;
 
 //Flash the icon and increase the block count
-function addOne()
+function addOne(tabId)
 {
 	blocked++;
 	chrome.browserAction.setBadgeText({text: '' + blocked});
-	chrome.browserAction.setBadgeBackgroundColor({color: [255,0,0,255]});
+	chrome.browserAction.setBadgeBackgroundColor({color: [255,255,0,255]});
 	chrome.browserAction.setIcon({path: "images/red.png"});
 	setTimeout(function(){
 		chrome.browserAction.setIcon({path: "images/green.png"});
 	}, 100);
 }
 
-var Agent = "Mozilla/5.0";
-
-//Non blocked requests
-var TrackedRequests = {};
-
-chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest, {urls: ["<all_urls>"]}, ["blocking"]);
 
 function onBeforeRequest(d) {
 	var domain = getDomain(d.url);
@@ -43,29 +50,35 @@ function onBeforeRequest(d) {
 	}
 }
 
-chrome.webRequest.onBeforeSendHeaders.addListener(onBeforeSendHeaders, {urls: ["<all_urls>"]}, ["requestHeaders", "blocking"]);
 
 function onBeforeSendHeaders(d) {
-
-	//Get header
-	var header = {};
-	for(var i in d.requestHeaders)
-	{
-		var h = d.requestHeaders[i];
-		if(h.name == "User-Agent")
-			header.UserAgent = h;
-		if(h.name == "Referer")
-			header.Referer = h;
-	}
-
-	//Set User-Agent
-	header.UserAgent.value = Agent;
 
 	//Get domain for target and referrer
 	var domain = getDomain(d.url);
 	var referrer;
-	if(header.Referer != undefined)
-		referrer = getDomain(header.Referer.value);
+	//Get header
+	var header = {};
+	for(var i = 0; i < d.requestHeaders.length; i++){
+		var h = d.requestHeaders[i];
+		if(h.name == "Referer")
+			referrer = getDomain(h.value);
+		if(h.name == "User-Agent")
+			h.value = "Mozilla/5.0";
+		if(h.name == "Accept")
+			h.value = "*/*";
+//		if(h.name == "Accept-Encoding")
+//			h.value = "gzip,deflate,sdch";
+		if(h.name == "Accept-Language"){
+			d.requestHeaders.splice(i, 1);
+			i--;
+			continue;
+		}
+		if(h.name == "Accept-Charset"){
+			d.requestHeaders.splice(i, 1);
+			i--;
+			continue;
+		}		
+	}
 
 	//Allow all within the same domain
 	if(domain === referrer)
@@ -110,7 +123,7 @@ function onBeforeSendHeaders(d) {
 			}
 		}
 	}
-
+		
 	//Save filter for onHeadersReceived below
 	requestFilter[d.requestId] = filter;
 	
@@ -118,13 +131,10 @@ function onBeforeSendHeaders(d) {
 	return {requestHeaders: d.requestHeaders};
 }
 
-//Track filter for every chrome request
-var requestFilter = {};
-
-chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived, {urls: ["<all_urls>"]}, ["responseHeaders"]);
 
 function onHeadersReceived(d){
 	var f = requestFilter[d.requestId];
+	delete requestFilter[d.requestId];
 
 	if(f == "clear"){
 		for(var i = 0; i < d.responseHeaders.length; i++){
