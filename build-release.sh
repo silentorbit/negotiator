9bf374e7e22aad43e72409331bdee2e30775adb8
Peter Hultqvist <hultqvist@silentorbit.com>
2015-09-05 16:23:21 +0200

    Prepared for WebExtension builds

--- build-release.sh
+++ build-release.sh
@@ -6,10 +6,16 @@ rm -r /tmp/Negotiator || true
 mkdir Negotiator
 rsync -rv $1 --exclude-from $1/release.exclude.txt Negotiator/ || true
 
-google-chrome --pack-extension=Negotiator --pack-extension-key=$2
+#Web Extension
+pushd Negotiator
+zip -r $1/Negotiator.xpi *
+popd
 
+#SilentOrbit
+google-chrome --pack-extension=Negotiator --pack-extension-key=$2
 mv Negotiator.crx $1/
 
+#Chrome Web Store
 #remove update_url for chrome web store
 cp Negotiator/manifest.json .
 cat manifest.json |grep -v update_url > Negotiator/manifest.json
