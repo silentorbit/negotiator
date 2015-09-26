c4bd8f983f8a5a125c6f3cdc9480e6ff638c990a
Peter Hultqvist <hultqvist@silentorbit.com>
2015-09-26 19:35:27 +0200

    Options style

--- script/page-options.js
+++ script/page-options.js
@@ -143,8 +143,8 @@ function addActionRow(a) {
         action.request = parseActionFilters(row.request.value);
         action.response = parseActionFilters(row.response.value);
         //Debug
-        row.querySelector("#requestParsed").textContent = JSON.stringify(action.request, null, "\t");
-        row.querySelector("#responseParsed").textContent = JSON.stringify(action.response, null, "\t");
+        //row.querySelector("#requestParsed").textContent = JSON.stringify(action.request, null, "\t");
+        //row.querySelector("#responseParsed").textContent = JSON.stringify(action.response, null, "\t");
         b.syncUpdateAction(a, action);
 
         row.style.backgroundColor = row.color.value;
