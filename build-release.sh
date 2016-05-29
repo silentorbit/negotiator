6f65b1c99fe8862d51853ed38bf877efb2e1c14f
Peter Hultqvist <peter@adductor.se>
2016-05-29 11:03:25 +0200

    Preparing version 8.1 for release.

--- build-release.sh
+++ build-release.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 set -e
+SOURCE=$(dirname $0)
+echo $SOURCE
+
+ls $1
+
 cd /tmp
 
 rm -r /tmp/Negotiator || true
 mkdir Negotiator
-rsync -rv $1 --exclude-from $1/release.exclude.txt Negotiator/ || true
+rsync -rv $SOURCE --exclude-from $SOURCE/release.exclude.txt Negotiator/ || true
 
 #Web Extension
 pushd Negotiator
-zip -r $1/Negotiator.xpi *
+zip -r $SOURCE/../Releases/Negotiator.xpi *
 popd
 
 #SilentOrbit
-google-chrome --pack-extension=Negotiator --pack-extension-key=$2
-mv Negotiator.crx $1/
+google-chrome --pack-extension=Negotiator --pack-extension-key=$1
+mv Negotiator.crx $SOURCE/../Releases/
 
 #Chrome Web Store
 #remove update_url for chrome web store
